92da52031377064555969d8b3c9eb13d76cb75b2
Roman Zhelnov <roman95zhelnov@gmail.com>
2024-07-29 17:55:26 +0300

    add

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,11 +1,18 @@
 export function searchImagesByQuery(query) {
-  const URL = 'https://pixabay.com/api/';
+  const URL = 'https://pixabay.com/api';
   const API_KEY = '45157582-00dbe423a45a27ad3340fe116';
-  fetch(
-    `${URL}?key${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`
-  ).then(response => {
+
+  const searchParams = new URLSearchParams({
+    key: API_KEY,
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+  });
+
+  return fetch(`${URL}?${searchParams}`).then(response => {
     if (!response.ok) {
-      throw new Error(response.status);
+      throw new Error('Network response was not ok');
     }
     return response.json();
   });
--- src/main.js
+++ src/main.js
@@ -1,14 +1,47 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 import { searchImagesByQuery } from './js/pixabay-api';
+import { renderImagesCards } from './js/render-functions';
 
-const form = document.querySelector('.gallery-form');
-const input = document.querySelector('.input-for-gallery');
-const loader = document.querySelector('.loader');
+const formEl = document.querySelector('.form-gallery');
+const inputEl = document.querySelector('.form-gallery-input');
+const loaderEl = document.querySelector('.loader');
+const galleryListEl = document.querySelector('.gallery-list');
 
-form.addEventListener('submit', handleSubmit);
+formEl.addEventListener('submit', handleSubmit);
 
 function handleSubmit(event) {
   event.preventDefault();
-  const inputValue = input.searchImagesByQuery();
+  galleryListEl.innerHTML = '';
+  loaderEl.classList.remove('visually-hidden');
+
+  const inputValue = inputEl.value.trim().toLowerCase();
+  if (inputValue === '') {
+    loaderEl.classList.add('visually-hidden');
+    return;
+  }
+  searchImagesByQuery(inputValue)
+    .then(images => {
+      loaderEl.classList.add('visually-hidden');
+      if (images.hits.length === 0) {
+        iziToast.error({
+          maxWidth: '370px',
+          position: 'topRight',
+          messageColor: 'white',
+          backgroundColor: 'red',
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+        });
+      } else {
+        loaderEl.classList.add('visually-hidden');
+        renderImagesCards(images, galleryListEl);
+      }
+    })
+    .catch(error => {
+      console.error(error);
+    })
+    .finally(() => {
+      inputEl.value = '';
+      form.reset();
+    });
 }
